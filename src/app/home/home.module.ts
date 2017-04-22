4334f49440f61a23b1669e2068339150cacea109
Lzjuhuenng <lzjuhuenng@Outlook.com>
2017-04-22 15:56:56 +0800

    删除 socia-channel组件

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { HomeComponent }   from './home.component';
-import { SocialChannelComponent } from './social-channel/social-channel.component';
 import { OnlineContactComponent } from './online-contact/online-contact.component';
 
 import {homeRoutes} from './home.routes';
@@ -15,7 +14,6 @@ import {homeRoutes} from './home.routes';
     exports: [],
     declarations: [
         HomeComponent,
-        SocialChannelComponent,
         OnlineContactComponent
     ],
     providers: [],
