2c7e75c8e2faa2d0e6283a754ab784c279cf9642
Lzjuhuenng <lzjuhuenng@Outlook.com>
2017-04-22 15:34:30 +0800

    删除postlist组件

--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -3,7 +3,6 @@ import { SharedModule} from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { PaginationModule } from 'ng2-bootstrap';
 
-import { PostlistService } from './postlist/services/postlist.service';
 import { AddCommentComponent } from '../comment/add-comment/add-comment.component';
 import { CommentService } from '../comment/services/comment.service';
 import { BooleanPipe } from '../utils/boolean-pipe';
@@ -23,7 +22,6 @@ import {postRoutes} from './post.routes';
         BooleanPipe
     ],
     providers: [
-        PostlistService,
         CommentService
     ]
 })
--- src/app/post/postlist/postlist.component.spec.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async } from '@angular/core/testing';
-import { PostlistComponent } from './postlist.component';
-
-describe('Component: Postlist', () => {
-	
-});
